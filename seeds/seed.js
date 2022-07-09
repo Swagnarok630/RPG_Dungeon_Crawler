1ef72c03bd31e02b9767393e026d30a3e799a832
Swagnarok <darryl.le.630@gmail.com>
2022-07-09 14:20:20 -0700

    quick

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { Chars, Items, Skills, Enemies } = require('../models');
+const { Chars, Enemies } = require('../models');
 const charData = require("./charsData.json");
-const itemData = require("./itemsData.json");
-const skillData = require("./skillsData.json");
+// const itemData = require("./itemsData.json");
+// const skillData = require("./skillsData.json");
 const enemyData = require("./enemiesData.json");
 
 // force: true will drop the table if it already exists
@@ -11,12 +11,12 @@ const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   const chars = await Chars.bulkCreate(charData);
-  const items = await Items.bulkCreate(itemData);
-  const skills = await Skills.bulkCreate(skillData);
+  // const items = await Items.bulkCreate(itemData);
+  // const skills = await Skills.bulkCreate(skillData);
   const enemies = await Enemies.bulkCreate(enemyData);
   console.log(`seeded ${chars.length} character data`);
-  console.log(`seeded ${items.length} item data`);
-  console.log(`seeded ${skills.length} skill data`);
+  // console.log(`seeded ${items.length} item data`);
+  // console.log(`seeded ${skills.length} skill data`);
   console.log(`seeded ${enemies.length} skill data`);
 
   process.exit(0)
